885d67e557d8e1abe5f160c8007f1f7e834cc2ac
Swati01012002 <swati2111010@akgec.ac.in>
2024-05-06 21:54:24 +0530

    Horizontal Scroll bar added.

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -8,6 +9,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   TextEditingController searchController = new TextEditingController();
+  List<String> navBarItem = ["Top News", "India", "World", "Finance", "Health"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,8 +61,65 @@ class _HomeState extends State<Home> {
               ],
             ),
           ),
+          Container(
+            height: 50,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: navBarItem.length,
+              itemBuilder: (context, index){
+                return InkWell(
+                  onTap: (){
+                    print(navBarItem[index]);
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Center(
+                      child: Text(navBarItem[index],
+                      style: TextStyle(
+                        fontSize: 19,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      ),),
+                    ),
+                  ),
+                );
+              }
+            ),
+          ),
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 200,
+              autoPlay: true,
+              enableInfiniteScroll: false,
+              enlargeCenterPage: true
+            ),
+            items: items.map((item){
+              return Builder(
+                builder: (BuildContext context){
+                  return InkWell(
+                    onTap: (){
+                      print("text");
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+                      decoration: BoxDecoration(
+                        color: item
+                      ),
+                    ),
+                  );
+                }
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
   }
+  final List items = [Colors.blueAccent, Colors.orange, Colors.red];
 }
